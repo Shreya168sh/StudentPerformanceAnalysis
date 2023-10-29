24350bcd6c7c159935d06b1b6ebe49f3e47b5123
Shreya Sharma <shreyas@audaxlabs.com>
2023-10-29 21:53:52 +0530

    Updated main file

--- main.py
+++ main.py
@@ -47,4 +47,4 @@ def predict(request: InputData):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='127.0.0.1', port=8084, reload=True)
+    uvicorn.run("main:app", host='localhost', port=8084)
